ba6c2bd482bae08a6beddd4d8cc02954bed7cab5
김건우 <papawolf@gimgeon-uui-MacBook-Pro.local>
2020-02-25 21:17:48 +0900

    add comment about 7 RESTful routes make partials make skeletons /post

--- index.js
+++ index.js
@@ -2,15 +2,52 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-var port = 4000;
 
 bodyParser.urlencoded({ extended: false });
 app.set("view engine", "ejs");
 
+
+// ====================
+//
+// ====================
+
+// ====================
+// 7 RESTful ROUTES
+// ====================
 app.get("/", function(req, res) {
-  res.render("home");
+  res.redirect("post");
 });
 
-app.listen(port, function() {
+// ROUTE 1: INDEX
+app.get("/post", function(req, res) {
+  res.render("post");
+});
+// ROUTE 2: NEW
+app.get("/post/new", function(req, res) {
+
+});
+// ROUTE 3: CREATE
+app.post("/post", function(req, res) {
+
+});
+// ROUTE 4: SHOW not VIEW
+app.get("/post:id", function(req, res) {
+
+});
+// ROUTE 5: EDIT
+app.get("/post:id/edit", function(req, res) {
+
+});
+// ROUTE 6: UPDATE
+app.put("/post:id", function(req, res) {
+
+});
+// ROUTE 7: DESTROY
+app.delete("/post:id", function(req, res) {
+
+});
+
+// LISTEN
+app.listen(process.env.PORT || 3000, function() {
   console.log("RESTful Blog begins");
 });
