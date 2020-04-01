850798e674b3a4a8c4e91bd7d2f98edb668c3663
김건우 <papawolf@gimgeon-uui-MacBook-Pro.local>
2020-04-01 21:34:11 +0900

    add in new and create

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var bodyParser = require("body-parser"),
 mongoose       = require("mongoose"),
 express        = require("express"),
@@ -7,24 +6,22 @@ app            = express();
 // ====================
 // APP CONFIG
 // ====================
-
 mongoose.connect("mongodb://localhost/restful_blog_app", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 app.set("view engine", "ejs");
-app.use(express.static("public")); // can use our custom css
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public")); // order to use our custom css
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // ====================
 // DB CONFIG - Schema, Model
 // ====================
-
 var blogSchema = new mongoose.Schema({
   title: String,
   image: String,
   body: String,
-created: {type: Date, default: Date.now}
+  created: {type: Date, default: Date.now}
 });
 
 var Blog = mongoose.model("Blog", blogSchema);
@@ -37,18 +34,14 @@ var Blog = mongoose.model("Blog", blogSchema);
 
 
 // ====================
-//
-// ====================
-
-// ====================
-// 7 RESTful ROUTES
+// RESTful ROUTES
 // ====================
 app.get("/", function(req, res) {
-  res.redirect("blog");
+  res.redirect("/blogs");
 });
 
 // ROUTE 1: INDEX
-app.get("/blog", function(req, res) {
+app.get("/blogs", function(req, res) {
   Blog.find({}, function(err, blogs) {
     if (err) {
       console.log(err);
@@ -58,31 +51,41 @@ app.get("/blog", function(req, res) {
   });
 });
 // ROUTE 2: NEW
-app.get("/blog/new", function(req, res) {
-
+app.get("/blogs/new", function(req, res) {
+  res.render("new");
 });
 // ROUTE 3: CREATE
-app.post("/blog", function(req, res) {
-
+app.post("/blogs", function(req, res) {
+  //create blog
+  Blog.create(req.body.blog, function(err, newBlog) {
+    if(err) {
+      res.redirect("new");
+    } else {
+      res.redirect("/blogs");
+    }
+  });
+  //then, redirect to index
 });
-// ROUTE 4: SHOW not VIEW
-app.get("/blog:id", function(req, res) {
+// ROUTE 4: SHOW
+app.get("/blogs:id", function(req, res) {
 
 });
 // ROUTE 5: EDIT
-app.get("/blog:id/edit", function(req, res) {
+app.get("/blogs:id/edit", function(req, res) {
 
 });
 // ROUTE 6: UPDATE
-app.put("/blog:id", function(req, res) {
+app.put("/blogs:id", function(req, res) {
 
 });
 // ROUTE 7: DESTROY
-app.delete("/blog:id", function(req, res) {
+app.delete("/blogs:id", function(req, res) {
 
 });
 
-// LISTEN
+// ====================
+// PORT & IP LISTEN
+// ====================
 app.listen(process.env.PORT || 3000, function() {
   console.log("RESTful Blog begins");
 });
